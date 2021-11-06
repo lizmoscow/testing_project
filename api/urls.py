92d34204301148768b391d67fe3ba36edb9ee3c4
Elizaveta Moskovskaya <liza_moskovskaya@Elizavetas-MacBook-Air.local>
2021-11-07 02:48:24 +0300

    Can visit and leave rooms (full)

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 
 
 urlpatterns = [
     path('home', RoomView.as_view()),
     path('create_room', CreateRoomView.as_view()),
+    path('get_room', GetRoom.as_view()),
+    path('join_room', JoinRoom.as_view()),
+    path('user_in_room', UserInRoom.as_view()),
+    path('leave_room', LeaveRoom.as_view()),
 ]
